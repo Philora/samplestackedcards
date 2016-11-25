b688e6ebb590c4e01a58f818bf1f0a1c4395338b
Stalon Giles <stalongiles@Stalons-MacBook-Air.local>
2016-11-25 11:32:45 +0530

    first commit

---
+++ app/src/main/java/com/app/samplestackedcards/BlankActivity.java
@@ -0,0 +1,14 @@
+package com.app.samplestackedcards;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BlankActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+    }
+
+}
